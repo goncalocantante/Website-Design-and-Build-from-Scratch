48bc600b30d57139ca2fa83fa5b8480a07aeb215
goncalocantante <goncalocantante@gmail.com>
2021-03-10 18:30:04 +0000

    Spacing Fixed. Some JS done.

--- customjs.js
+++ customjs.js
@@ -1,7 +1,6 @@
 /* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
-  
 
   if (currentScrollPos > 500) {
     document.getElementById("title").style.display = "none";
@@ -10,5 +9,11 @@ window.onscroll = function() {
     document.getElementById("title").style.display = "block";
     console.log(currentScrollPos)
   }
+  if (currentScrollPos > 750) {
+    $('.modal').modal('show'); 
+  } else{
+    $('.modal').modal('hide'); 
+  }
+
 
 }
